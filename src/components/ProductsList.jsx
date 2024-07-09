6ea7448c6d04e75bf994b1854a7df692071ed241
KokotheKat <aglusa2020@gmail.com>
2024-07-09 19:07:05 -0300

    feat: add visual

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,34 +1,107 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchProducts } from '../store/slices/productsSlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchProducts } from "../store/slices/productsSlice";
+import styled from "styled-components";
+
+const ProductListStyle = styled.div``;
+const ProductWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+  justify-content: center;
+`;
+const ProductItem = styled.article`
+  padding: 1rem;
+  width: 100%;
+  max-width: 20rem;
+  height: 30rem;
+  background-color: #fff;
+  color: #000;
+  border-radius: 2rem;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProductImage = styled.img`
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  height: auto;
+  object-fit: contain;
+`;
+const ProductTitle = styled.h3`
+  font-size: 2em;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+const ProductDescription = styled.p`
+  font-size: 1em;
+`;
+const ProductPrice = styled.span`
+  font-size: 1.5em;
+`;
+const ProductRating = styled.span``;
+const ProductComments = styled.span`
+  font-size: 0.9em;
+  color: #ccc;
+`;
+const ProductBadge = styled.span`
+  font-size: 1.5em;
+`;
+const ProductOffert = styled.span``;
+const ProductAddCart = styled(Link)`
+  font-size: 1em;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  border: none;
+  cursor: pointer;
+`;
+const ProductAddFavorite = styled.button`
+  width: 1rem;
+  height: 1rem;
+  background-color: red;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+`;
+const ProductTag = styled.button``;
 
 export const ProductsList = () => {
-	const dispatch = useDispatch();
-	const products = useSelector((state) => state.products.items);
-	const productStatus = useSelector((state) => state.products.status);
-	const error = useSelector((state) => state.products.error);
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.items);
+  const productStatus = useSelector((state) => state.products.status);
+  const error = useSelector((state) => state.products.error);
 
-	useEffect(() => {
-		if (productStatus === 'idle') {
-			dispatch(fetchProducts());
-		}
-	}, [productStatus, dispatch]);
+  useEffect(() => {
+    if (productStatus === "idle") {
+      dispatch(fetchProducts());
+    }
+  }, [productStatus, dispatch]);
 
-	if (productStatus === 'loading' || productStatus === 'idle') return <p>Cargando productos...</p>;
-	if (productStatus === 'failed') return <p>Error: {error}</p>;
+  if (productStatus === "loading" || productStatus === "idle")
+    return <p>Cargando productos...</p>;
+  if (productStatus === "failed") return <p>Error: {error}</p>;
 
-	return (
-		<section>
-			<h2>Productos</h2>
-			<ul>
-				{products.map((product) => (
-					<li key={product.id}>
-						<Link to={`${product.id}`}>{product.id} {product.name}</Link>
-					</li>
-				))}
-			</ul>
-		</section>
-	);
+  return (
+    <ProductListStyle>
+      <h2>Productos</h2>
+      <ProductWrapper>
+        {products.map((product) => (
+          <ProductItem key={product.id}>
+            <ProductImage src={product.image} alt={product.title} />
+            <ProductTitle>{product.title}</ProductTitle>
+            <ProductDescription>{product.decription}</ProductDescription>
+            <ProductPrice>{product.price}</ProductPrice>
+            <ProductAddCart to={`${product.id}`}>
+              Agregar al carrito
+            </ProductAddCart>
+          </ProductItem>
+        ))}
+      </ProductWrapper>
+    </ProductListStyle>
+  );
 };
-
--- src/store/slices/productsSlice.js
+++ src/store/slices/productsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
-	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const response = await fetch('https://fakestoreapi.com/products');
 	return response.json();
 });
 
